75acf22f1682bd0b34e2582b0d32caf3bbaffb65
inciner8r <punarvpawade17@gmail.com>
2023-02-04 22:53:52 +0530

    comments

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"net/http"
 )
 
+// handler function
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
+	//register http handler function to root url
 	http.HandleFunc("/", hello)
+
+	//serve on port 8080
 	http.ListenAndServe(":8080", nil)
 }
